3e0fab951e23574b6485acbce33fbb67557dd37a
Anatolii Olkhovskyi <anatoltbcm@gmail.com>
2025-03-12 03:37:25 +0200

    lesson 3

--- src/main/java/com/javarush/telegram/TinderBoltApp.java
+++ src/main/java/com/javarush/telegram/TinderBoltApp.java
@@ -1,22 +1,28 @@
 package com.javarush.telegram;
 
 import static com.javarush.telegram.Util.fetchMainMenuData;
+import static com.javarush.telegram.Util.getDateButtonsData;
+import static com.javarush.telegram.Util.getMessageButtonsData;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import java.util.ArrayList;
+import java.util.List;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class TinderBoltApp extends MultiSessionTelegramBot {
     private static final Dotenv dotenv = Dotenv.load();
+    private static final String TELEGRAM_BOT_NAME = dotenv.get("TELEGRAM_BOT_NAME");
+    private static final String TELEGRAM_BOT_TOKEN = dotenv.get("TELEGRAM_BOT_TOKEN");
+    private static final String OPEN_AI_TOKEN = dotenv.get("OPEN_AI_TOKEN");
 
-    public static final String TELEGRAM_BOT_NAME = dotenv.get("TELEGRAM_BOT_NAME");
-    public static final String TELEGRAM_BOT_TOKEN = dotenv.get("TELEGRAM_BOT_TOKEN");
-    public static final String OPEN_AI_TOKEN = dotenv.get("OPEN_AI_TOKEN");
+    private static final ChatGPTService gptService = new ChatGPTService(OPEN_AI_TOKEN);
 
-    public DialogMode dialogMode = DialogMode.MAIN;
-    public ChatGPTService gptService = new ChatGPTService(OPEN_AI_TOKEN);
+    private DialogMode dialogMode = DialogMode.MAIN;
+    private List<String> chatHistory;
 
     public TinderBoltApp() {
         super(TELEGRAM_BOT_NAME, TELEGRAM_BOT_TOKEN);
@@ -24,9 +30,9 @@ public class TinderBoltApp extends MultiSessionTelegramBot {
 
     @Override
     public void onUpdateEventReceived(Update update) {
-        String message = getMessageText();
+        String callBackMessage = getMessageText();
 
-        if (message.equals("/start")) {
+        if (callBackMessage.equals("/start")) {
             dialogMode = DialogMode.MAIN;
             String menu = loadMessage("main");
             showMainMenu(fetchMainMenuData(menu));
@@ -35,20 +41,71 @@ public class TinderBoltApp extends MultiSessionTelegramBot {
             return;
         }
 
-        if (message.equals("/gpt")) {
+        if (callBackMessage.equals("/gpt")) {
             dialogMode = DialogMode.GPT;
-            String gptMessage = loadMessage("gpt");
-            sendTextMessage(gptMessage);
+            String message = loadMessage("gpt");
+            sendTextMessage(message);
             sendPhotoMessage("gpt");
             return;
         }
 
         if (dialogMode == DialogMode.GPT) {
+            Message waitingMessage = sendWaitingMessage();
             String prompt = loadPrompt("gpt");
-            String string = gptService.sendMessage(prompt, message);
-            sendTextMessage(string);
+            String gtpAnswer = gptService.sendMessage(prompt, callBackMessage);
+            updateTextMessage(waitingMessage, gtpAnswer);
             return;
         }
+
+        if (callBackMessage.equals("/date")) {
+            dialogMode = DialogMode.DATE;
+            sendPhotoMessage("date");
+            String message = loadMessage("date");
+            sendTextButtonsMessage(message, getDateButtonsData());
+            return;
+        }
+
+        if (dialogMode == DialogMode.DATE) {
+            String callBackButtonKey = getCallbackQueryButtonKey();
+            if (callBackButtonKey.startsWith("date_")) {
+                sendPhotoMessage(callBackButtonKey);
+                sendTextMessage("Починай спілкування. ✍");
+                String prompt = loadPrompt(callBackButtonKey);
+                gptService.setPrompt(prompt);
+                return;
+            }
+            Message waitingMessage = sendWaitingMessage();
+            String gptAnswer = gptService.addMessage(callBackMessage);
+            updateTextMessage(waitingMessage, gptAnswer);
+            return;
+        }
+
+        if (callBackMessage.equals("/message")) {
+            dialogMode = DialogMode.MESSAGE;
+            sendPhotoMessage("message");
+            String message = loadMessage("message");
+            sendTextButtonsMessage(message, getMessageButtonsData());
+            chatHistory = new ArrayList<>();
+            return;
+        }
+
+        if (dialogMode == DialogMode.MESSAGE) {
+            String callBackButtonKey = getCallbackQueryButtonKey();
+            if (callBackButtonKey.startsWith("message_")) {
+                Message waitMessage = sendWaitingMessage();
+                String prompt = loadPrompt(callBackButtonKey);
+                String history = String.join("/n/n", chatHistory);
+                String gptAnswer = gptService.sendMessage(prompt, history);
+                updateTextMessage(waitMessage, gptAnswer);
+                return;
+            }
+            chatHistory.add(callBackMessage);
+            return;
+        }
+    }
+
+    private Message sendWaitingMessage() {
+        return sendTextMessage("Почекай.");
     }
 
     public static void main(String[] args) throws TelegramApiException {
--- src/main/java/com/javarush/telegram/Util.java
+++ src/main/java/com/javarush/telegram/Util.java
@@ -1,24 +1,48 @@
 package com.javarush.telegram;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public final class Util {
 
     public static String[] fetchMainMenuData(String origin) {
-        String[] commands = Arrays.stream(
+        String[] descriptionsCommands = Arrays.stream(
                         origin
                                 .split("\\*Корисні команди та посилання:\\*")[1]
-                                .split("(\\d+. | - |\\r\\n)"))
+                                .split("(\\d+\\. | - |\\r\\n)"))
                 .filter(s -> !s.isBlank())
                 .toArray(String[]::new);
 
-        for (int i = 0; i < commands.length; i += 2) {
-            String temp = commands[i];
-            commands[i] = commands[i + 1];
-            commands[i + 1] = temp;
+        for (int i = 0; i < descriptionsCommands.length; i += 2) {
+            String temp = descriptionsCommands[i];
+            descriptionsCommands[i] = descriptionsCommands[i + 1];
+            descriptionsCommands[i + 1] = temp;
         }
 
-        return commands;
+        return descriptionsCommands;
+    }
+
+    public static List<String> getDateButtonsData() {
+        List<String> starsIds = new ArrayList<>();
+        starsIds.add("Аріана Гранде 🔥");
+        starsIds.add("date_grande");
+        starsIds.add("Марго Роббі 🔥🔥");
+        starsIds.add("date_robbie");
+        starsIds.add("Зендея 🔥🔥🔥");
+        starsIds.add("date_zendaya");
+        starsIds.add("Райан Гослінг 😎");
+        starsIds.add("date_gosling");
+        starsIds.add("Том Харді 😎😎");
+        starsIds.add("date_hardy");
+        return starsIds;
+    }
+
+    public static String[] getMessageButtonsData() {
+        return new String[]{
+                "Наступне повідомлення", "message_next",
+                "Запросити на побачення", "message_date"
+        };
     }
 
     private Util() {
